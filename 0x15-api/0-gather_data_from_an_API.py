463cbb4036f04e3b95e1d518d0abf207bb44dc80
areignel <48579497+areignel@users.noreply.github.com>
2023-04-24 22:22:02 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
 """
-Using https://jsonplaceholder.typicode.com
-returns info about employee TODO progress
-Implemented using recursion
+For a given employee ID, returns information about his/her TODO list progress,
+using an API.
 """
-import re
 import requests
-import sys
+from sys import argv
 
+URL = "https://jsonplaceholder.typicode.com"  # The API's URL
 
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
-
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+if __name__ == "__main__":
+    if len(argv) > 1:
+        if argv[1].isdecimal() and int(argv[1]) >= 0:
+            emp_id = int(argv[1])
+            u_resp = requests.get('{}/users/{}'.format(URL, emp_id)).json()
+            t_resp = requests.get('{}/todos'.format(URL)).json()
+            emp_name = u_resp.get('name')
+            todos = [t for t in t_resp if t.get('userId') == emp_id]
+            completed = [t for t in todos if t.get('completed')]
+            print('Employee {} is done with tasks({}/{}):'.format(
+                        emp_name,
+                        len(completed),
+                        len(todos)))
+            for t in completed:
+                print('\t {}'.format(t.get('title')))
